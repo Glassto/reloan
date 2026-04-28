6e017b00a1dfd77fdb06d6f9cbb560425d9f28a9
Glassto <benjaamin.maciuc28@gmail.com>
2026-04-28 19:07:57 +0300

    implemented routing and navigation

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import {Href, Link} from "expo-router";
+
+const SignIn = () => {
+    return (
+        <View>
+            <Text>SignIn</Text>
+            <Link href={"/(auth)/sign-in" as Href}>Sign In</Link>
+        </View>
+    );
+};
+
+export default SignIn;
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import "@/global.css"
 import { Text, View } from "react-native";
+import {Href, Link} from "expo-router";
 
 export default function App() {
     return (
@@ -7,6 +8,10 @@ export default function App() {
             <Text className="text-xl font-bold text-success">
                 Welcome to Nativewind!
             </Text>
+            <Link href="/onboarding" className={"mt-4 rounded-xl w-98 text-white text-center bg-primary p-4"}>Go to Onboarding  </Link>
+            <Link href={"/(auth)/sign-in" as Href} className={"mt-4 rounded-lg text-white bg-primary p-4"}>Sign in  </Link>
+            <Link href={"/(auth)/sign-up" as Href} className={"mt-4 rounded-lg text-white bg-primary p-4"}>Sign up  </Link>
+            <Link href={"/subscriptions/spotify" as Href}>Spotify Subscription</Link>
         </View>
     );
 }
